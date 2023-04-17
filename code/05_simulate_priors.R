6a33dce3546b23b7f81d4dd25bd3486ba8d0cb8a
yitalu <luyita.tw@gmail.com>
2023-04-17 15:29:15 +0200

    update

--- code/05_simulate_priors.R
+++ code/05_simulate_priors.R
@@ -30,16 +30,28 @@ bV10 <- rnorm(N, mean_b, sigma_b)
 # Simulate Confirmed cases ------------------------------------------------
 
 # Distribution of virus concentration
-v_sim <- runif(N, 1, 1)
+v_sim <- runif(N, 0, 0)
 
 
 # Infection probabilities
 prob <- inv_logit(a + bV1 * lag(v_sim, 1) + bV2 * lag(v_sim, 2) + bV3 * lag(v_sim, 3) + bV4 * lag(v_sim, 4) + bV5 * lag(v_sim, 5) + bV6 * lag(v_sim, 6) + bV7 * lag(v_sim, 7) + bV8 * lag(v_sim, 8) + bV9 * lag(v_sim, 9) + bV10 * lag(v_sim, 10))
 summary(prob)
-dens(prob, adj = 0.1, main = "probability distribution")
+
+# pdf("./figures/prior_prediction_prob_uniform.pdf", width = 9, height = 6)
+# pdf("./figures/prior_prediction_prob_max.pdf", width = 9, height = 6)
+# pdf("./figures/prior_prediction_prob_min.pdf", width = 9, height = 6)
+dens(prob, adj = 0.1, lwd = 2, main = "Distribution of Infection Probability (Virus Concentration: Minimum)")
+grid()
+# dev.off()
 
 
 # Simulate Confirmed Cases
 c_sim <- rbinom(N, 3.3e5, prob)
 summary(c_sim)
-dens(c_sim, adj = 0.1, main = "confirmed case")
+
+# pdf("./figures/prior_prediction_case_uniform.pdf", width = 9, height = 6)
+# pdf("./figures/prior_prediction_case_max.pdf", width = 9, height = 6)
+# pdf("./figures/prior_prediction_case_min.pdf", width = 9, height = 6)
+dens(c_sim, adj = 0.1, lwd = 2, main = "Distribution of Confirmed Case (Virus Concentration: Minimum)")
+grid()
+# dev.off()
